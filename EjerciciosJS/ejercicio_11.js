11929b36f87eecad5c4e9df0c18e2e7f8c599cfc
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-03-02 17:31:50 -0500

    Termine el ejercicio_11

---
+++ EjerciciosJS/ejercicio_11.js
@@ -0,0 +1,12 @@
+// 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+
+const factorial = (numero) => {
+  let contador = 1;
+  for (let i = 1; i <= numero; i++) {
+    contador = contador * i;
+  }
+
+  return contador;
+};
+
+console.log(factorial(5));
