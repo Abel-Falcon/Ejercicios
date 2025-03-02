1597d20d7cac4d8c4644a38ef61a11a392746156
Abel-Falcon <nehemias.falcon@upeu.edu.pe>
2025-03-02 14:17:39 -0500

    Octavo ejercicio resuelto

---
+++ EjerciciosJS/ejercicio_08.js
@@ -0,0 +1,12 @@
+const miFuncion = (arreglo, buscar) => {
+  let ec = new RegExp(buscar, "ig");
+  let index = arreglo.match(ec).length;
+
+  for (let i = 0; i < index; i++) {
+    arreglo = arreglo.replace(buscar, "");
+  }
+
+  return arreglo;
+};
+
+console.log(miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz"));
